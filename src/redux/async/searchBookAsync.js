9ae2dbdb27193df886c143d3b300a990d88a838e
pranshul <pranshul.evdtechnology@gmail.com>
2024-11-19 12:10:36 +0530

    update(console)

--- src/redux/async/searchBookAsync.js
+++ src/redux/async/searchBookAsync.js
@@ -4,8 +4,6 @@ import {AxiosClient} from "../AxiosClient";
 export const searchBookAsync = createAsyncThunk(
   "get/new/book",
   async ({payload, params}, toolkit) => {
-    console.log("payload...", payload, "params", params);
-
     return await AxiosClient(
       "GET",
       `/volumes?q=${payload}`,
